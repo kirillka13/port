da60c163ecf94f25b04aa79ee7cbe2156d752d6f
kirillka13 <155494449+kirillka13@users.noreply.github.com>
2024-03-26 22:38:40 +0000

    Failed attempt at implementing a rudimentary search feature into the website.
    No other description is needed.

---
+++ script.js
@@ -0,0 +1,7 @@
+var search = document.getElementById("search");
+var button = document.getElementById("button");
+button.onclick = function() {
+  var searchitem = document.getElementById('search').value;
+  alert(searchitem);
+
+}
